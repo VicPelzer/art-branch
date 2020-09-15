f0ba759eddd5a8395ac637bc1025d9e911c7b15a
VPelzer <71321068+VicPelzer@users.noreply.github.com>
2020-09-15 15:41:31 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  // put setup code here
+  createCanvas(200, 200);
+  background(100);
+  print("dit is de setup in de print functie");
+
+  ellipseMode(CENTER);
+  ellipse(100, 100, 200, 200);
+  fill(255,0,0);
+  rect(100,100,20,20);
+
+
+  textAlign(CENTER, CENTER);
+  text("Dit is het midden", 100, 100);
+  
+  
+  
+}
+
+function draw() {
+  // put drawing code here
+
+ 
+
+  // random(255) -> 0
+}
